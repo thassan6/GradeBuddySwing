fda637d8dfd01d7ad1f10e2b5df2d30c86d99336
Mornelasagy <mornelas@masonlive.gmu.edu>
2019-11-14 20:18:00 -0500

    the following changed were made to Panel1: * changed the width of the text fields * name, prof name, and course name delete the original text when clicked * course number and goal grade highlight the entire text to make it easier to enter data

--- src/Panel1.java
+++ src/Panel1.java
@@ -1,29 +1,73 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
 import java.awt.event.ActionListener;
+
 
 public class Panel1 extends JPanel implements ActionListener {
 
     JTextField name, professorName, courseName, numAssignmentTypes, goalGrade;
     JButton next;
+    Course course;
 
     public Panel1(){
+        course = new Course();
 
         setLayout(null);
         setBounds(0, 0, 600, 800);
 
         // initialize components
         name = new JTextField("Name");
-        name.setBounds(350, 150, 75, 30);
+        name.setBounds(350, 150, 100, 30);
+        name.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if( name.getText().equals("Name")) {
+                    name.setText("");
+                }
+            }
+        });
+        
         professorName = new JTextField("Professor");
-        professorName.setBounds(350, 190, 75, 30);
+        professorName.setBounds(350, 190, 100, 30);
+        professorName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if( professorName.getText().equals("Professor")) {
+                    professorName.setText("");
+                }
+            }
+        });
+        
         courseName = new JTextField("Course Name");
-        courseName.setBounds(350, 230, 75, 30);
+        courseName.setBounds(350, 230, 100, 30);
+        courseName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if( courseName.getText().equals("Course Name")) {
+                    courseName.setText("");
+                }
+            }
+        });
+        
         numAssignmentTypes = new JTextField("3");
-        numAssignmentTypes.setBounds(350, 270, 75, 30);
-        goalGrade = new JTextField("4.0");
-        goalGrade.setBounds(350, 310, 75, 30);
-
+        numAssignmentTypes.setBounds(350, 270, 100, 30);
+        numAssignmentTypes.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                numAssignmentTypes.selectAll();
+            }
+        });
+        
+        goalGrade = new JTextField("100.0");
+        goalGrade.setBounds(350, 310, 100, 30);
+        goalGrade.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e)
+            {
+                goalGrade.selectAll();
+            }
+        });
+        
         next = new JButton("Next");
         next.setBounds(350, 360, 75, 30);
         next.addActionListener(this);
@@ -39,28 +83,28 @@ public class Panel1 extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+       
         // save number of assignments for ease of use.
         int numAss = Integer.parseInt(numAssignmentTypes.getText());
 
         // save all course info in course object
-        // initialize Course arrays
-        Login frame = (Login) SwingUtilities.getWindowAncestor(this);
-        Course.numAssignmentTypes = numAss;
-        Course.courseName = courseName.getText();
-        Course.goalGrade = Double.parseDouble(goalGrade.getText());
-        Course.professor = professorName.getText();
-        Course.yourName = name.getText();
+        // initialize Course weights[] and courseNames[]
+        course.numAssignmentTypes = numAss;
+        course.courseName = courseName.getText();
+        course.goalGrade = Double.parseDouble(goalGrade.getText());
+        course.professor = professorName.getText();
+        course.yourName = name.getText();
         Course.initializeWeightsArray(numAss);
-        Course.initializeAssignmentTypeNames(numAss);
-        Course.initializeGrades(numAss);
+        course.initializeCourseNames(numAss);
 
-
-        // initialize all the arrays in panel2/3 and next button
+        Login frame = (Login) SwingUtilities.getWindowAncestor(this);
+        
+        // initialize all the arrays in panel2 and next button
         frame.panel2.assignmentNames = new JTextField[numAss];
         frame.panel2.assignmentQuantities = new JTextField[numAss];
         frame.panel2.assignmentWeights = new JTextField[numAss];
-        frame.panel3.grades = new JTextField[numAss][];
-
+        
+        //if (e.getSource() == name) name.selectAll();
 
         // dynamically create all the TextFields for Panel2
         int ypos = 75;
@@ -81,21 +125,24 @@ public class Panel1 extends JPanel implements ActionListener {
             frame.panel2.add(temp2);
             frame.panel2.add(temp3);
 
+            frame.panel2.revalidate();
+            frame.panel2.repaint();
+
             ypos+=50;
+
+            setVisible(false);
+            frame.panel2.setVisible(true);
         }
 
         // set bounds of next button under everything
         frame.panel2.next.setBounds(350, ypos, 75, 30);
         frame.panel2.revalidate();
 
-        setVisible(false);
-        frame.panel2.setVisible(true);
-
         //DEBUG
-//        JOptionPane.showMessageDialog(null, "DEBUG: course.numAssignmentTypes\n"+course.numAssignmentTypes);
-//        JOptionPane.showMessageDialog(null, "DEBUG: course.courseName\n"+course.courseName);
-//        JOptionPane.showMessageDialog(null, "DEBUG: course.goalGrade\n"+course.goalGrade);
-//        JOptionPane.showMessageDialog(null, "DEBUG: course.professor\n"+course.professor);
-//        JOptionPane.showMessageDialog(null, "DEBUG: course.yourName\n"+course.yourName);
+        JOptionPane.showMessageDialog(null, "DEBUG: course.numAssignmentTypes\n"+course.numAssignmentTypes);
+        JOptionPane.showMessageDialog(null, "DEBUG: course.courseName\n"+course.courseName);
+        JOptionPane.showMessageDialog(null, "DEBUG: course.goalGrade\n"+course.goalGrade);
+        JOptionPane.showMessageDialog(null, "DEBUG: course.professor\n"+course.professor);
+        JOptionPane.showMessageDialog(null, "DEBUG: course.yourName\n"+course.yourName);
     }
 }
